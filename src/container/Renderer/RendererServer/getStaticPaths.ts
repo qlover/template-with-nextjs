cc3cc7f4dca97eca84f8ec36502084b126dd3546
qrj <myused@sina.com>
2022-12-12 14:08:56 +0800

    update getStaticPaths

--- src/container/Renderer/RendererServer/getStaticPaths.ts
+++ src/container/Renderer/RendererServer/getStaticPaths.ts
@@ -1,69 +1,98 @@
-import { has } from 'lodash';
-import { GetStaticPathsContext, GetStaticPathsResult } from 'next/types';
-import { ParsedUrlQuery } from 'querystring';
-import { BaseConfigType, ErrorHandler } from '.';
-import { prepareForSerializatoin } from '../../../utils/next-utils/prepareForSerializatoin';
+import appConfig from '@/config/appConfig';
+import { prepareForSerializatoin } from '@/utils/next-utils/prepareForSerializatoin';
+import { parseQSForString } from '@/utils/next-utils/wrapperContext';
+import {
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from 'next/types';
+import RenderDispatch, {
+  BaseConfigType,
+  ErrorHandler,
+  plugRouterFilter,
+} from '.';
 
-type HandlerType<Props extends ParsedUrlQuery = ParsedUrlQuery> = (
-  context: GetStaticPathsContext
-) => Promise<GetStaticPathsResult<Props> | void | Props>;
+type HandlerType<Props> = (
+  context: GetServerSidePropsContext
+) => Promise<GetServerSidePropsResult<Props> | void | Props>;
 
 /**
- * 是否是一个 GetStaticPropsResult 返回结果
+ * 是否是一个 ssr 返回结果
  * @param val
  * @returns
  */
-function isNextResult<P extends ParsedUrlQuery = ParsedUrlQuery>(
-  val: any
-): val is GetStaticPathsResult<P> {
-  return has(val, 'paths') && has(val, 'fallback');
+function isNextResult<P>(val: any): val is GetServerSidePropsResult<P> {
+  return val.props || val.redirect || val.notFound;
 }
 
 async function wrapperHandler<P extends PlainObject>(
-  context: GetStaticPathsContext,
+  context: GetServerSidePropsContext,
   config: BaseConfigType<HandlerType<P>>
 ) {
-  let result = {
-    paths: [],
-    fallback: false,
-  } as GetStaticPathsResult<P>;
+  const { resolvedUrl, locale } = context;
+
+  const {
+    jsonTransform = appConfig.appEnv !== 'master',
+    routerFilter = appConfig.appEnv !== 'local',
+  } = config;
+
+  let result = { props: { empty: true } as P | PlainObject };
+
+  routerFilter && plugRouterFilter(resolvedUrl, locale);
+
   if (config?.handler) {
     const props = await config.handler(context);
     if (props && isNextResult<P>(props)) {
-      return prepareForSerializatoin(props);
+      return jsonTransform ? prepareForSerializatoin(props) : props;
     }
+
+    result.props = { ...result.props, ...props };
   }
+
+  if (jsonTransform) {
+    return prepareForSerializatoin(result);
+  }
+
   return result;
 }
 
-export default function getStaticPaths<
-  P extends ParsedUrlQuery = ParsedUrlQuery
->(config: BaseConfigType<HandlerType<P>> = {}) {
-  // const { catchError = appConfig.appEnv !== 'local' } = config;
+/**
+ * getServerSideProps 包裹
+ *
+ * `handler`
+ *    - 可以随意返回一个对象，会当作 props
+ *    - 可以按规则返回会被 next 直接使用
+ *    - 还可以什么都不返回，为了开发方便, 默认 返回 `props:{ empty:true }`
+ *    - 可手动抛出 `ServerRendererError` 由 `ErrorHandler` 处理
+ *
+ * @param config
+ * @returns
+ */
+export default function getServerSideProps<P extends PlainObject>(
+  config: BaseConfigType<HandlerType<P>> = {}
+): (context: GetServerSidePropsContext) => Promise<any> {
+  const { catchError = appConfig.appEnv !== 'fork:test' } = config;
 
-  return async function getStaticPaths(context: GetStaticPathsContext) {
+  return async function getServerSideProps(context: GetServerSidePropsContext) {
     try {
-      // const { locale } = context;
+      const { locale } = context;
 
-      // // 更新 server state
-      // RenderDispatch.state = {
-      //   // @ts-expect-error
-      //   query: context.params,
-      //   client: {
-      //     locale: (locale as LocalApp.Lang) || appConfig.lang,
-      //   },
-      // };
+      // 更新 server state
+      RenderDispatch.state = {
+        query: parseQSForString(context.query),
+        client: {
+          locale: (locale as LocalApp.Lang) || appConfig.lang,
+        },
+      };
 
-      const result = await wrapperHandler<P>(context, config);
-      return result;
+      return await wrapperHandler<P>(context, config);
     } catch (e) {
-      console.log('[RenderDispatch getStaticPaths error]', e);
+      console.log('[DispatechRender Error]', e);
 
-      // if (catchError) {
-      return ErrorHandler(e as Error);
-      // }
+      if (catchError) {
+        return ErrorHandler(e as Error);
+      }
     } finally {
-      // RenderDispatch.clearState();
+      RenderDispatch.clearState();
     }
   };
 }
